25688bc29bffb45e38ed0fb1890d8ed16a4b256a
LAZ-R <crazydrawer62@gmail.com>
2026-05-14 15:02:04 +0200

    v 0.0.0.2 - fix line height mobile

--- app-properties.js
+++ app-properties.js
@@ -1,5 +1,5 @@
 export const APP_NAME = "OMU - Générateur de devis et contrats";
-export const APP_VERSION = '0.0.0.1';
+export const APP_VERSION = '0.0.0.2';
 export const APP_LOCAL_STORAGE_ID = `omu-gdc-0001`;
 
 /* 
@@ -7,5 +7,6 @@ export const APP_LOCAL_STORAGE_ID = `omu-gdc-0001`;
 
 # CHANGELOG ###############################################
 
+v 0.0.0.2 - fix line height mobile
 v 0.0.0.1 - init
 */
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const version = '0.0.0.1';
+const version = '0.0.0.2';
 const CACHE_NAME = `omu-gdc-${version}`;
 
 const APP_SHELL = [
